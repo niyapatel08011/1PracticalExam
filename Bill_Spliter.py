6727955c0f8297f947724db85782f991f0069ec9
niyapatel08011 <niyapatel08011@gmail.com>
2026-06-27 18:34:39 +0530

    Add files via upload

--- Bill_Spliter.py
+++ Bill_Spliter.py
@@ -1,23 +1,35 @@
-while True:
-  print("Welcome to Bill Spliter App..!")
-  print("")
- 
-  bill=float(input("Enter Total Bill Amount:"))
-  people=int(input("Enter Number Of People:"))
-  tip=float(input("Enter Tip Percentage(0/5/10/15/20):"))
-  print("")
 
-  Tip_amount=(tip/100)*bill
-  print(f"Tip Amount:{Tip_amount}")
-  print("")
-  Total_bill=bill+tip
-  print(f"Total Bill(with tip):{Total_bill}")
-  print("")
-  per_person=Total_bill/people
-  print(f"Per Person:{per_person}")
-  print("")
-  again=input("Do you Want to split another bill?(yes/no):")
-  if again.lower()!="yes":
-   break
-  print("Thank you for using bill Spliter")
-  continue
+while True:
+    print("WELCOME TO BLL SPLITTER APP..!")
+ 
+    print("")
+ 
+    bill=float(input("Enter Total Bill Amount:- "))
+    people=int(input("Enter Number Of People:- "))
+    tip=float(input("Enter tip percentage(0/5/10/15/20):- "))
+    print("")
+
+    Tip_amount=(tip/100)*bill    
+    print(f"Tip Amount:- {Tip_amount}")
+    
+
+    Total_bill= bill+tip
+    print(f"Total Bill(with tip):- {Total_bill}")
+    
+
+    per_person=Total_bill/people
+    print(f"Per Person:- {per_person}")
+    print("")
+        
+    again=input("Would you like to calculate another bill? (yes/no) :")
+    print("")
+    if again.lower()!="yes":
+      print("THANK YOU! for using bil Splitter. ")
+      break
+    continue
+    
+    
+
+    
+
+  
